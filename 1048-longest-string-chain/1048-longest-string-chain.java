534e1ad7e308ccbbbf82655b71cbfb9f562e712f
Anurag <148947882+Student-Anurag@users.noreply.github.com>
2026-08-17 13:14:20 +0530

    Time: 44 ms (71.26%), Space: 47.1 MB (27.42%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,33 +1,18 @@
 class Solution {
-    public boolean isPredecessor(String a, String b) {
-        if(a.length() != 1 + b.length()) return false;
-        int i = a.length() - 1, j = b.length() - 1;
-        int mismatchAllowed = 0;
-        while(i >= 0 && j >= 0) {
-            if(a.charAt(i) == b.charAt(j)) {
-                i--;    j--;
-            }
-            else {
-                mismatchAllowed++;
-                i--;
-            }
-        }
-        return mismatchAllowed <= 1;
-    }
     public int longestStrChain(String[] words) {
-        int n = words.length;
-        // sort in basis of size
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
-        int[] LCS = new int[n];
-        Arrays.fill(LCS, 1);
+        Arrays.sort(words, Comparator.comparingInt(String::length));
+        Map<String, Integer> dp = new HashMap<>();
         int maxLen = 1;
-        for(int i=1; i<n; i++) {
-            for(int j=i-1; j>=0; j--) {
-                if(isPredecessor(words[i], words[j])) {
-                    LCS[i] = Math.max(LCS[i], 1 + LCS[j]);
+        for(String word: words) {
+            int current = 1;
+            for(int i=0; i<word.length(); i++) {
+                String predecessor = word.substring(0, i) + word.substring(i+1);
+                if(dp.containsKey(predecessor)) {
+                    current = Math.max(current, 1 + dp.get(predecessor));
                 }
             }
-            maxLen = Math.max(maxLen, LCS[i]);
+            dp.put(word, current);
+            maxLen = Math.max(maxLen, current);
         }
         return maxLen;
     }
