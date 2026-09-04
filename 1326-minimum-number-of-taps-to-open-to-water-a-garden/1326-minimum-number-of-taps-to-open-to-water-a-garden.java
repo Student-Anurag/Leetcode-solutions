c8b304cde9e903d60591e684db135e1f5612fbdf
Anurag <148947882+Student-Anurag@users.noreply.github.com>
2026-09-04 13:38:32 +0530

    Time: 696 ms (5.13%), Space: 230 MB (7.34%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -1,20 +1,42 @@
 class Solution {
+    Map<String, Integer> dp = new HashMap<>();
+    public int solve(int i, int maxEnd, List<int[]> range, int n) {
+        // Already covered the entire garden
+        if(maxEnd >= n) return 0;
+        // No more intervals
+        if(i >= range.size()) {
+            return Integer.MAX_VALUE;
+        }
+        // There is a gap so we can not cover the garden
+        if(range.get(i)[0] > maxEnd) {
+            return Integer.MAX_VALUE;
+        }
+        String key = i + "," + maxEnd;
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
+        // Don't open this tap
+        int not_open = solve(i+1, maxEnd, range, n);
+        // Open this tap
+        int next = solve(i+1, Math.max(maxEnd, range.get(i)[1]), range, n);
+        int open = Integer.MAX_VALUE;
+        // Prevent Integer.MAX_VALUE + 1 overflow
+        if(next != Integer.MAX_VALUE) {
+            open = 1 + next;
+        }
+        int ans = Math.min(not_open, open);
+        dp.put(key, ans);
+        return ans;
+    }
     public int minTaps(int n, int[] ranges) {
-        int[] startEnd = new int[n+1];
+        List<int[]> range = new ArrayList<>();
         for(int i=0; i<ranges.length; i++) {
-            int start = Math.max(0, i - ranges[i]);
-            int end = Math.min(n, i + ranges[i]);
-            startEnd[start] = Math.max(startEnd[start], end);
+            int start = Math.max(0, i-ranges[i]);
+            int end = Math.min(n, i+ranges[i]);
+            range.add(new int[]{start, end});
         }
-        int taps = 0, currEnd = 0, maxEnd = 0;
-        for(int i=0; i<n+1; i++) {
-            if(i > maxEnd) return -1;
-            if(i > currEnd) {
-                taps++;
-                currEnd = maxEnd;
-            }
-            maxEnd = Math.max(maxEnd, startEnd[i]);
-        }
-        return taps;
+        range.sort((a, b) -> Integer.compare(a[0], b[0]));
+        int ans = solve(0, 0, range, n);
+        return (ans == Integer.MAX_VALUE) ? -1 : ans;
     }
 }
